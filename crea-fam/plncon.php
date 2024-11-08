b2df8e21fb699a2c74ecb9baf98f3e8f2bd8e057
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-08 14:39:04 -0500

    11-08 Update plncon.php 14:38

--- crea-fam/plncon.php
+++ crea-fam/plncon.php
@@ -1,3 +1,4 @@
+
 <?php
 ini_set('display_errors','1');
 require_once "../libs/gestion.php";
@@ -47,8 +48,7 @@ $total=$info['responseResult'][0]['total'];
 $regxPag=5;
 $pag=(isset($_POST['pag-compConc']))? ($_POST['pag-compConc']-1)* $regxPag:0;
 
-    $sql="SELECT concat(idviv,'_',idcon) ACCIONES, idcon AS Cod_Compromiso,compromiso,
-        FN_CATALOGODESC(26,equipo) 'Equipo',cumple
+    $sql="SELECT concat(idviv,'_',idcon) ACCIONES, idcon AS Cod_Compromiso,fecha,FN_CATALOGODESC(26,equipo) 'Equipo'
         FROM `hog_planconc` 
             WHERE idviv='".$id[0];
         $sql.="' ORDER BY fecha_create";
@@ -68,9 +68,9 @@ function cmp_compConc(){
 	$o='compConc';
 	$c[]=new cmp($o,'e',null,'PLAN DE CUIDADO FAMILIAR CONCERTADO',$w);
   $c[]=new cmp('idp','h',15,$_POST['id'],$w.' '.$key.' '.$o,'id','id',null,'####',false,false);
-	$c[]=new cmp('obs','a',50,$e,$w.' '.$o,'Compromisos concertados','observaciones',null,null,true,true,'','col-7');
-	$c[]=new cmp('equipo','s','3',$e,$w.' '.$o,'Equipo que concerta','equipo',null,null,true,true,'','col-2');
-	$c[]=new cmp('cumplio','o','2',$e,$w.' '.$o,'cumplio','cumplio',null,null,false,true,'','col-1');
+  $c[]=new cmp('fecha','d','3',$e,$w.' '.$o,'Fecha de concertación','fecha',null,null,true,true,'','col-5');
+	$c[]=new cmp('equipo','s','3',$e,$w.' '.$o,'Perfil que concerta','equipo',null,null,true,true,'','col-5');
+  $c[]=new cmp('obs','a',50,$e,$w.' '.$o,'Compromisos concertados','observaciones',null,null,true,true,'','col-0');
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	$rta .="<div class='encabezado placuifam'>TABLA DE COMPROMISOS CONCERTADOS</div>
 	<div class='contenido' id='compConc-lis' >".lis_compConc()."</div></div>";
@@ -85,9 +85,9 @@ function gra_compConc(){
       $params = [
         ['type' => 'i', 'value' => NULL ],
         ['type' => 's', 'value' => $id[0]],
-        ['type' => 's', 'value' => $_POST['obs']],
+        ['type' => 's', 'value' => $_POST['fecha']],
         ['type' => 'i', 'value' => $_POST['equipo']],
-        ['type' => 's', 'value' => $_POST['cumplio']],
+        ['type' => 's', 'value' => $_POST['obs']],
         ['type' => 's', 'value' => date("Y-m-d H:i:s")],
         ['type' => 'i', 'value' => $_SESSION['us_sds']],
         ['type' => 's', 'value' => ''],
@@ -96,66 +96,29 @@ function gra_compConc(){
       ];
       $rta = mysql_prepd($sql, $params);
     }else{
-    $sql="UPDATE hog_planconc SET cumple=?,fecha_update=?,usu_update=? WHERE idcon=?"; //  compromiso=?, equipo=?, 
+   /*  $sql="UPDATE hog_planconc SET cumple=?,fecha_update=?,usu_update=? WHERE idcon=?"; //  compromiso=?, equipo=?, 
     $params = [
         ['type' => 's', 'value' => $_POST['cumplio']],
         ['type' => 's', 'value' => date("Y-m-d H:i:s")],
         ['type' => 'i', 'value' => $_SESSION['us_sds']],
         ['type' => 'i', 'value' => $id[1]]
       ];
-      $rta = mysql_prepd($sql, $params);
+      $rta = mysql_prepd($sql, $params); */
     }
 return $rta;
 }
 
 
 	function get_compConc(){
-        // print_r($_POST);
-        /* if (!$_POST['id']) {
-            return '';
-        }
-        $id = divide($_POST['id']);
-        if(count($id)==2){
-          $sql = "SELECT concat(idviv,'_',idcon) 'id',compromiso,equipo,cumple
-                FROM `hog_planconc` 
-                WHERE idviv='{$id[0]}' AND idcon='{$id[1]}'
-                LIMIT 1";
-        // echo $sql;		
-          $info = datos_mysql($sql);
-        }else{
-          $sql = "SELECT idviv 'id',compromiso,equipo,cumple
-                FROM `hog_planconc` 
-                WHERE idviv='{$id[0]}'
-                LIMIT 1";
-                echo $sql;
-                $info = datos_mysql($sql);
-        }
-        if (!$info['responseResult']) {
-          return '';
-
-        }else{
-          return $info['responseResult'][0];
-          
-        } */
-
-
-
-
         if($_REQUEST['id']==''){
           return "";
         }else{
           // print_r($_POST);
           $id=divide($_REQUEST['id']);
           // print_r($id);
-          $sql="SELECT concat(idviv,'_',idcon) 'id',compromiso,equipo,cumple
+          $sql="SELECT concat(idviv,'_',idcon) 'id',fecha,equipo,compromiso
                 FROM `hog_planconc` 
                 WHERE idviv='{$id[0]}' AND idcon='{$id[1]}'";
-          // echo $sql;
-          // LEFT JOIN personas P ON F.tipo_doc=P.tipo_doc AND F.documento=P.idpersona
-          // LEFT JOIN hog_viv H ON P.vivipersona = H.idviv
-          // LEF JOIN ( SELECT CONCAT(estrategia, '_', sector_catastral, '_', nummanzana, '_', predio_num, '_', unidad_habit, '_', estado_v) AS geo, direccion, localidad, barrio
-                    // FROM hog_geo ) AS G ON H.idgeo = G.geo
-          // print_r($id);
           $info=datos_mysql($sql);
            return json_encode($info['responseResult'][0]);
             } 
@@ -221,6 +184,17 @@ return $rta;
         function opc_equipo($id=''){
             return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=26 and estado='A' ORDER BY 1",$id);
         } 
+      
+        function segSi($id) {
+          $id = divide($id); // Asumimos que divide() retorna un array
+          $sql = "SELECT estado_seg cumple FROM hog_segcom WHERE id_con='" . $id[1] . "'";
+          $info = datos_mysql($sql);
+          if (isset($info['responseResult'][0]['cumple']) && $info['responseResult'][0]['cumple'] == 1) {
+              return false;  
+          } else {
+              return true;
+          }
+      }
 
 
 	function formato_dato($a,$b,$c,$d){
@@ -229,7 +203,10 @@ return $rta;
         // var_dump($a);
 		if ($a=='compConc' && $b=='acciones'){
 			$rta="<nav class='menu right'>";
-				$rta.="<li class='icono editar ' title='Editar Compromiso' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getDataFetch,500,'compConc',event,this,'plncon.php',['obs','equipo']);\"></li>";  //   act_lista(f,this);
+				$rta.="<li title='Ver Compromiso'><i class='fa-solid fa-eye ico' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getDataFetch,500,'compConc',event,this,'plncon.php',['fecha','obs','equipo']);\"></i></li>";  //   act_lista(f,this);
+        if (segSi($c['ACCIONES'])) {
+          $rta.="<li ><i class='fa-solid fa-house-medical-circle-check ico' title='Seguimiento a Compromisos' id='".$c['ACCIONES']."' Onclick=\"mostrar('segComp','pro',event,'','plnsegcon.php',7,'Seguimiento a Compromisos');\"></i></li>";
+        }
 			}
 		return $rta;
 	}
